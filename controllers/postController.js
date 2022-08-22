fb755f1a94ac6139eecc481663059e4ba2b27c67
dcamilovelandia <deybi.velandia@bodytechcorp.com>
2022-08-22 15:39:39 -0500

    added get post by id

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken')
 const PostModel = require('../models/vehicles/post_model')
 
 module.exports = {
-    async createPost( req, res ) {
+    async createPost(req, res) {
         let token = req.headers.authorization.split(' ')[1];
-            const decodeToken = jwt.verify(token, process.env.TOKEN_SECRET)
+        const decodeToken = jwt.verify(token, process.env.TOKEN_SECRET)
 
         console.log(decodeToken)
         try {
@@ -43,18 +43,63 @@ module.exports = {
                 created_by: decodeToken.id
             })
 
-            await post.save().then( response => {
+            await post.save().then(response => {
                 res.status(201).json({
                     success: true,
                     message: 'post created',
                     data: response ?? {}
                 })
-            }).catch( err => {
+            }).catch(err => {
                 validations.validateResponse(res, err)
             })
 
-        } catch(e) {
+        } catch (e) {
             validations.validateResponse(res, 'Error while create post')
+        }
+    },
+
+    async getPotByID(req, res) {
+        try {
+            let token = req.headers.authorization.split(' ')[1];
+            const decodeToken = jwt.verify(token, process.env.TOKEN_SECRET)
+
+            const { id } = req.query
+
+            const getPost = await PostModel.findById({ _id: id })
+
+            if (getPost) {
+
+                if (!getPost.view_count.includes(decodeToken.id)) {
+
+                    await PostModel.findByIdAndUpdate({ _id: id }, {
+                        $push: {
+                            view_count: decodeToken.id
+                        }
+                    }, { new: true }).then( response => {
+                        res.status(200).json({
+                            success: true,
+                            message: 'data found',
+                            data: response
+                        })
+                    }).catch(err => {
+                        res.status(200).json({
+                            success: true,
+                            message: 'data found',
+                            data: getPost
+                        })
+                    })
+                }
+            
+            } else {
+                res.status(200).json({
+                    success: true,
+                    message: 'No data found',
+                    data: {}
+                })
+            }
+
+        } catch (e) {
+            validations.validateResponse(res, 'Error while getting post')
         }
     }
 }
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,5 +3,6 @@ const validateRole = require('../middleware/validateRole')
 const passport = require('passport')
 
 module.exports = (app) => {
-    app.post('/api/post/create', [passport.authenticate('jwt', {session: false}), validateRole.validateAdmin], PostController.createPost)
+    app.post('/api/post/create', [passport.authenticate('jwt', {session: false}), validateRole.validateAdmin], PostController.createPost),
+    app.get('/api/post/getById', passport.authenticate('jwt', {session: false}), PostController.getPotByID)
 }
