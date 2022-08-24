e1cb9b71653bb031e3c9d54c5a1ba7e419ec621a
dcamilovelandia <deybi.velandia@bodytechcorp.com>
2022-08-24 15:51:37 -0500

    added finf by name

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -4,24 +4,47 @@ const PostModel = require('../models/vehicles/post_model')
 const BrandModel = require('../models/vehicles/brand_model')
 
 module.exports = {
-    async filterByBrand( req, res ) {
+    async filterByBrand(req, res) {
         try {
             const { brand_id } = req.query
-            await PostModel.find({ brand: brand_id }, { images:{$slice:  1}}).select([
-                'name',
-                'price',
-                '{ images:{$slice:  1}'
-            ]).then( response => {
+            await PostModel.find({ brand: brand_id }, { images: { $slice: 1 } }).select([
+                '{ images:{$slice:  1}',
+                "name",
+                "price",
+                "data_sheet.release_date",
+                "data_sheet.mileage",
+                "concessionarie_location",
+                "is_new"
+            ]).then(response => {
                 res.status(200).json({
                     success: true,
                     message: 'getting by brands',
                     data: response
                 })
-            }).catch( err => {
+            }).catch(err => {
                 validations.validateResponse(res, err)
             })
-        } catch(e) {
+        } catch (e) {
             validations.validateResponse(res, e ?? 'Error finding by brand')
+        }
+    },
+
+    async filterbyName( req, res ) {
+        try {
+            const { text } = req.query
+            await PostModel.find({ $text: {
+                $search: text
+            }}).then( response => {
+                res.status(200).json({
+                    success: true,
+                    message: 'searching complete',
+                    data: response ?? {}
+                }).catch( err => {
+                    validations.validateResponse(res, err)
+                })
+            })
+        } catch(e) {
+            validations.validateResponse(res, e ?? 'Error searching by name')
         }
     }
 }
--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -3,5 +3,6 @@ const passport = require('passport')
 const SearchController = require('../controllers/searchController')
 
 module.exports = (app) => {
-    app.get('/api/search/findByBrand', passport.authenticate('jwt', {session: false}), SearchController.filterByBrand)
+    app.get('/api/search/findByBrand', passport.authenticate('jwt', {session: false}), SearchController.filterByBrand),
+    app.get('/api/search/findByName', passport.authenticate('jwt', {session: false}), SearchController.filterbyName)
 }
