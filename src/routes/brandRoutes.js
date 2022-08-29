6c87cdfce7f756b7e9901444abcc12e7404c32ad
dcamilovelandia <deybi.velandia@bodytechcorp.com>
2022-08-29 15:20:44 -0500

    1) fix passport

--- src/routes/brandRoutes.js
+++ src/routes/brandRoutes.js
@@ -1,11 +1,12 @@
 const BrandController = require('../controllers/brandController')
 const validateRole = require('../middleware/validateRole')
 const jwt_helper = require('../helpers/jwt_helper')
+const passport = require('passport')
 
 module.exports = (app) => {
-    app.post('/api/brand/create', [jwt_helper.verifyAccessToken, validateRole.validateAdmin], BrandController.createBrand)
-    app.put('/api/brand/update', [jwt_helper.verifyAccessToken, validateRole.validateAdmin], BrandController.updateBrand)
-    app.delete('/api/brand/delete', [jwt_helper.verifyAccessToken, validateRole.validateAdmin], BrandController.deleteBrand)
-    app.get('/api/brand/getAll', [jwt_helper.verifyAccessToken, validateRole.validateAdmin], BrandController.getAllBrands)
-    app.get('/api/brand/getById', [jwt_helper.verifyAccessToken, validateRole.validateAdmin], BrandController.getBrandById)
+    app.post('/api/brand/create', [jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), validateRole.validateAdmin], BrandController.createBrand)
+    app.put('/api/brand/update', [jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), validateRole.validateAdmin], BrandController.updateBrand)
+    app.delete('/api/brand/delete', [jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), validateRole.validateAdmin], BrandController.deleteBrand)
+    app.get('/api/brand/getAll', [jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), validateRole.validateAdmin], BrandController.getAllBrands)
+    app.get('/api/brand/getById', [jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), validateRole.validateAdmin], BrandController.getBrandById)
 }
--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -1,8 +1,9 @@
 const jwt_helper = require('../helpers/jwt_helper')
+const passport = require('passport')
 const SearchController = require('../controllers/searchController')
 
 module.exports = (app) => {
-    app.get('/api/search/findByBrand', jwt_helper.verifyAccessToken, SearchController.filterByBrand),
-    app.get('/api/search/findByName', jwt_helper.verifyAccessToken, SearchController.filterbyName),
-    app.get('/api/search/findByVehivleType', jwt_helper.verifyAccessToken, SearchController.filterByVehicleType)
+    app.get('/api/search/findByBrand', jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), SearchController.filterByBrand),
+    app.get('/api/search/findByName', jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), SearchController.filterbyName),
+    app.get('/api/search/findByVehivleType', jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), SearchController.filterByVehicleType)
 }
