30a26ef397006e6834f1c95bdcc64d51ab58fb5f
dcamilovelandia <deybi.velandia@bodytechcorp.com>
2022-08-30 12:08:36 -0500

    1) deleted upload middleaware

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,12 +2,11 @@ const PostController = require('../controllers/postController')
 const validateRole = require('../middleware/validateRole')
 const jwt_helper = require('../helpers/jwt_helper')
 const passport = require('passport')
-const { upload } = require('../middleware/uploadFile')
 
 
 
 module.exports = (app) => {
-    app.post('/api/post/create', [jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), validateRole.validateAdmin, upload.array('image')], PostController.createPost),
+    app.post('/api/post/create', [jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), validateRole.validateAdmin], PostController.createPost),
         app.get('/api/post/getById', jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}),PostController.getPotByID),
         app.get('/api/post/getAll', jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), PostController.getAll),
         app.delete('/api/post/delete', [jwt_helper.verifyAccessToken, passport.authenticate('jwt', {session: false}), validateRole.validateAdmin], PostController.deletePost),
